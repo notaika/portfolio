db218ada7fa1877ed176f96e793434e207e760c9
notaika <ercamanalo@gmail.com>
2024-06-28 13:55:57 -0700

    responsive design mobile to desktop done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,14 @@ import './Header.scss'
 
 export default function Header() {
   return (
-    <section className="header">
-        
+    <section className="header" id="header">
+        <div className="header__intro">
+            <p className="header__intro-text header__intro-wrap">Hi, my name is</p>
+            <h2 className="header__intro-name header__intro-wrap">Aika Manalo</h2>
+            <h2 className="header__intro-job header__intro-wrap">A Software Developer</h2>
+            <p className="header__intro-location header__intro-wrap">based in Vancouver, Canada</p>
+        </div>
+        <a href="" className="header-btn">Let's Connect!</a>
     </section>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import './Navbar.scss';
 import navbarLogo from '../../assets/logos/aika-logo.png';
+import aboutIcon from '../../assets/icons/user.png';
+import projectsIcon from '../../assets/icons/folder.png';
+import contactIcon from '../../assets/icons/communication.png';
+import downloadIcon from '../../assets/icons/download.png';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-        <img src={navbarLogo} alt="Aika logo for the navbar that links to home" className="navbar__logo" />
+        <a href="#header" className="navbar__logo-link"><img src={navbarLogo} alt="Aika logo for the navbar that links to home" className="navbar__logo" /></a>
         <ul className="navbar__list">
-            <li className="navbar__list-item">About</li>
-            <li className="navbar__list-item">Projects</li>
-            <li className="navbar__list-item">Contact</li>
-            <li className="navbar__list-item">Resume</li>
+            <li className="navbar__list-item">
+                <img src={aboutIcon} alt="" className="navbar__list-item-icon" />
+                <a href="" className="navbar__list-text">About</a>
+                </li>
+            <li className="navbar__list-item">
+                <img src={projectsIcon} alt="" className="navbar__list-item-icon" />
+                <a href="" className="navbar__list-text">Projects</a></li>
+            <li className="navbar__list-item">
+                <img src={contactIcon} alt="" className="navbar__list-item-icon" />
+                <a href="" className="navbar__list-text">Contact</a>
+            </li>
+            <li className="navbar__list-item">
+                <img src={downloadIcon} alt="" className="navbar__list-item-icon" />
+                <a href="" className="navbar__list-text">Resume</a>
+                </li>
         </ul>
     </nav>
   )
