4aa61460f6d0dca8ce656de4f506eeee214eaa56
notaika <ercamanalo@gmail.com>
2024-06-28 15:11:20 -0700

    fix: responsive ratios

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import './Header.scss'
+import laptopImage from '../../assets/images/laptop.png';
+import speechBubble from '../../assets/images/bubble-chat.png';
+import './Header.scss';
 
 export default function Header() {
   return (
     <section className="header" id="header">
-        <div className="header__intro">
-            <p className="header__intro-text header__intro-wrap">Hi, my name is</p>
-            <h2 className="header__intro-name header__intro-wrap">Aika Manalo</h2>
-            <h2 className="header__intro-job header__intro-wrap">A Software Developer</h2>
-            <p className="header__intro-location header__intro-wrap">based in Vancouver, Canada</p>
+        <div className="header__hero">
+            <div className="header__intro">
+                <p className="header__intro-text header__intro-wrap">Hi, my name is</p>
+                <h2 className="header__intro-name header__intro-wrap">Aika Manalo</h2>
+                <h2 className="header__intro-job header__intro-wrap">A Software Developer</h2>
+                <p className="header__intro-location header__intro-wrap">based in Vancouver, Canada</p>
+            </div>
+            <div className="header__hero-speech"> </div>
+            <a href="" className="header-btn">Let's Connect!</a>
         </div>
-        <a href="" className="header-btn">Let's Connect!</a>
     </section>
   )
 }
